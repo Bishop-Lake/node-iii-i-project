88079eb877ec7a216c72cd8d1109e4b300aa9290
Bishop <lake.d.bishop@gmail.com>
2019-09-16 16:41:34 -0500

    Well everything works, MVP Complete

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const db = require('./database/db-config.js')
 
 const Users = require('./users/users-model.js');
 
-const restricted = require('./auth/middleware.js');
+const withAuth = require('./auth/middleware.js');
 
 const server = express();
 
@@ -16,6 +16,51 @@ server.use(express.json());
 server.get('/', (req, res) => {
     res.send("Sanity check");
 });
+
+server.post('/api/register', (req, res) => {
+    let { username, password } = req.body;
+  
+    const hash = bcrypt.hashSync(password, 12);
+  
+    Users.add({ username, password: hash })
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+  server.post('/api/login', (req, res) => {
+    let { username, password } = req.body;
+  
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          res.status(401).json({ message: 'You cannot pass!' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+  server.get('/api/users', withAuth, (req, res) => {
+    Users.find()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  });
+  
+  server.get('/hash', (req, res) => {
+    const name = req.query.name;
+    const hash = bcrypt.hashSync(name, 8);
+    res.send(`the hash for ${name} is ${hash}`);
+  });
 
 //end endpoints
 
